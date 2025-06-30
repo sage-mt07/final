49af005d2765ba3e410985164cfc3bddeae3ce57
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-07-01 01:05:13 +0900

    Fix CreateDbTopicAsync parameter usage and update tests

--- src/Infrastructure/Admin/KafkaAdminService.cs
+++ src/Infrastructure/Admin/KafkaAdminService.cs
@@ -144,7 +144,10 @@ internal class KafkaAdminService : IDisposable
         if (replicationFactor <= 0)
             throw new ArgumentException("replicationFactor must be > 0", nameof(replicationFactor));
 
-        var topics = await _adminClient.DescribeTopicsAsync(new[] { topicName });
+        var topics = await _adminClient.DescribeTopicsAsync(
+            new TopicCollection(new[] { topicName }),
+            null,
+            CancellationToken.None);
         var desc = topics.FirstOrDefault();
         if (desc != null)
         {
--- tests/Infrastructure/KafkaAdminServiceTests.cs
+++ tests/Infrastructure/KafkaAdminServiceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 using Kafka.Ksql.Linq.Configuration;
@@ -16,7 +17,7 @@ public class KafkaAdminServiceTests
 {
     private class FakeAdminClient : DispatchProxy
     {
-        public Func<IEnumerable<string>, Task<List<TopicMetadata>>> DescribeHandler { get; set; } = _ => Task.FromResult(new List<TopicMetadata>());
+        public Func<TopicCollection, DescribeTopicsOptions?, CancellationToken, Task<List<TopicMetadata>>> DescribeHandler { get; set; } = (_, __, ___) => Task.FromResult(new List<TopicMetadata>());
         public Func<IEnumerable<TopicSpecification>, CreateTopicsOptions?, Task> CreateHandler { get; set; } = (_, __) => Task.CompletedTask;
 
         protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
@@ -26,7 +27,10 @@ public class KafkaAdminServiceTests
                 case nameof(IAdminClient.CreateTopicsAsync):
                     return CreateHandler((IEnumerable<TopicSpecification>)args![0]!, (CreateTopicsOptions?)args[1]);
                 case nameof(IAdminClient.DescribeTopicsAsync):
-                    return DescribeHandler((IEnumerable<string>)args![0]!);
+                    return DescribeHandler(
+                        (TopicCollection)args![0]!,
+                        (DescribeTopicsOptions?)args[1]!,
+                        (CancellationToken)args[2]!);
                 case "Dispose":
                     return null;
                 case "get_Name":
@@ -114,7 +118,7 @@ public class KafkaAdminServiceTests
         var proxy = DispatchProxy.Create<IAdminClient, FakeAdminClient>();
         var fake = (FakeAdminClient)proxy!;
         var created = false;
-        fake.DescribeHandler = _ => Task.FromResult(new List<TopicMetadata>());
+        fake.DescribeHandler = (_, __, ___) => Task.FromResult(new List<TopicMetadata>());
         fake.CreateHandler = (_, __) => { created = true; return Task.CompletedTask; };
 
         var svc = CreateUninitialized(options, proxy);
@@ -130,7 +134,7 @@ public class KafkaAdminServiceTests
         var proxy = DispatchProxy.Create<IAdminClient, FakeAdminClient>();
         var fake = (FakeAdminClient)proxy!;
         var meta = (TopicMetadata)RuntimeHelpers.GetUninitializedObject(typeof(TopicMetadata));
-        fake.DescribeHandler = _ => Task.FromResult(new List<TopicMetadata> { meta });
+        fake.DescribeHandler = (_, __, ___) => Task.FromResult(new List<TopicMetadata> { meta });
         var created = false;
         fake.CreateHandler = (_, __) => { created = true; return Task.CompletedTask; };
 
@@ -146,7 +150,7 @@ public class KafkaAdminServiceTests
         var options = new KsqlDslOptions();
         var proxy = DispatchProxy.Create<IAdminClient, FakeAdminClient>();
         var fake = (FakeAdminClient)proxy!;
-        fake.DescribeHandler = _ => Task.FromResult(new List<TopicMetadata>());
+        fake.DescribeHandler = (_, __, ___) => Task.FromResult(new List<TopicMetadata>());
         fake.CreateHandler = (_, __) => throw new KafkaException(new Error(ErrorCode.Local_Transport));
 
         var svc = CreateUninitialized(options, proxy);
